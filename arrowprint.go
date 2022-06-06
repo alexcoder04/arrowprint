77d577336e0d721f0ff73815c5bed20a1b93d6db
alexcoder04 <alexcoder04@protonmail.com>
2022-06-06 13:38:37 +0200

    prinf

--- arrowprint.go
+++ arrowprint.go
@@ -2,34 +2,34 @@ package arrowprint
 
 import "fmt"
 
-func printColor0(cc int, msg string) {
-	fmt.Printf("\033[3%dm==>\033[0m %s\n", cc, msg)
+func printColor0(cc int, msg string, args ...interface{}) {
+	fmt.Printf("\033[3%dm==>\033[0m %s\n", cc, fmt.Sprintf(msg, args...))
 }
 
-func printColor1(cc int, msg string) {
-	fmt.Printf("\033[3%dm  ->\033[0m %s\n", cc, msg)
+func printColor1(cc int, msg string, args ...interface{}) {
+	fmt.Printf("\033[3%dm  ->\033[0m %s\n", cc, fmt.Sprintf(msg, args...))
 }
 
-func Suc0(msg string) {
-	printColor0(2, msg)
+func Suc0(msg string, args ...interface{}) {
+	printColor0(2, msg, args...)
 }
 
-func Warn0(msg string) {
-	printColor0(3, msg)
+func Warn0(msg string, args ...interface{}) {
+	printColor0(3, msg, args...)
 }
 
-func Err0(msg string) {
-	printColor0(1, msg)
+func Err0(msg string, args ...interface{}) {
+	printColor0(1, msg, args...)
 }
 
-func Suc1(msg string) {
-	printColor1(2, msg)
+func Suc1(msg string, args ...interface{}) {
+	printColor1(2, msg, args...)
 }
 
-func Warn1(msg string) {
-	printColor1(3, msg)
+func Warn1(msg string, args ...interface{}) {
+	printColor1(3, msg, args...)
 }
 
-func Err1(msg string) {
-	printColor1(1, msg)
+func Err1(msg string, args ...interface{}) {
+	printColor1(1, msg, args...)
 }
