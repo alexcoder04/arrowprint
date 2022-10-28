1bb82705d01311a733175652e2fc84a31dc578a5
alexcoder04 <alexcoder04@protonmail.com>
2022-10-28 12:37:43 +0200

    change argument type to any

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 
 Use Arch-Linux-like output in your Go programs!
 
-Windows support through [mattn/go-colorable](https://github.com/mattn/go-colorable).
+## Features
+
+ - Different levels and colors of messages
+ - `printf`-like formatting support
+ - Windows support through [mattn/go-colorable](https://github.com/mattn/go-colorable).
 
 ## Example
 
@@ -11,9 +15,10 @@ Windows support through [mattn/go-colorable](https://github.com/mattn/go-colorab
 
 ```go
 arrowprint.InfoC("Starting program...")
-arrowprint.Suc0("Initializing system")
-arrowprint.Suc1("checking variables")
-arrowprint.Warn1("some variables are not defined, this may lead to errors")
+step1 := "system"
+arrowprint.Suc0("Initializing %s", step1)
+arrowprint.Suc1("checking %s", step1)
+arrowprint.Warn1("%s are not defined, this may lead to errors", "some variables")
 arrowprint.InfoC("Running...")
 arrowprint.Err0("An error occured, shutting down")
 ```
@@ -21,23 +26,24 @@ arrowprint.Err0("An error occured, shutting down")
 ## Available functions
 
 ```go
-// :: message
-func InfoC(msg string, args ...interface{}) { }
-func SucC(msg string, args ...interface{}) { }
-func WarnC(msg string, args ...interface{}) { }
-func ErrC(msg string, args ...interface{}) { }
+// colon print, bold
+// ` :: message`
+func InfoC(msg string, args ...any) { }
+func SucC(msg string, args ...any) { }
+func WarnC(msg string, args ...any) { }
+func ErrC(msg string, args ...any) { }
 
 // level 0 print
-// => message
-func Info0(msg string, args ...interface{}) { }
-func Suc0(msg string, args ...interface{}) { }
-func Warn0(msg string, args ...interface{}) { }
-func Err0(msg string, args ...interface{}) { }
+// ` => message`
+func Info0(msg string, args ...any) { }
+func Suc0(msg string, args ...any) { }
+func Warn0(msg string, args ...any) { }
+func Err0(msg string, args ...any) { }
 
 // level 1 print
-//   -> message
-func Info1(msg string, args ...interface{}) { }
-func Suc1(msg string, args ...interface{}) { }
-func Warn1(msg string, args ...interface{}) { }
-func Err1(msg string, args ...interface{}) { }
+// `   -> message`
+func Info1(msg string, args ...any) { }
+func Suc1(msg string, args ...any) { }
+func Warn1(msg string, args ...any) { }
+func Err1(msg string, args ...any) { }
 ```
--- arrowprint.go
+++ arrowprint.go
@@ -7,7 +7,7 @@ import (
 )
 
 // helper functions
-func printColor0(cc int, msg string, args ...interface{}) {
+func printColor0(cc int, msg string, args ...any) {
 	fmt.Fprintf(
 		colorable.NewColorableStdout(),
 		"\033[3%dm==>\033[0m %s\n",
@@ -15,7 +15,7 @@ func printColor0(cc int, msg string, args ...interface{}) {
 		fmt.Sprintf(msg, args...))
 }
 
-func printColor1(cc int, msg string, args ...interface{}) {
+func printColor1(cc int, msg string, args ...any) {
 	fmt.Fprintf(
 		colorable.NewColorableStdout(),
 		"\033[3%dm  ->\033[0m %s\n",
@@ -23,7 +23,7 @@ func printColor1(cc int, msg string, args ...interface{}) {
 		fmt.Sprintf(msg, args...))
 }
 
-func printColon(cc int, msg string, args ...interface{}) {
+func printColon(cc int, msg string, args ...any) {
 	fmt.Fprintf(
 		colorable.NewColorableStdout(),
 		"\033[3%d;1m ::\033[0m\033[1m %s\033[0m\n",
@@ -33,45 +33,45 @@ func printColon(cc int, msg string, args ...interface{}) {
 
 // colon print
 // :: Message
-func InfoC(msg string, args ...interface{}) {
+func InfoC(msg string, args ...any) {
 	printColon(4, msg, args...)
 }
-func SucC(msg string, args ...interface{}) {
+func SucC(msg string, args ...any) {
 	printColon(2, msg, args...)
 }
-func WarnC(msg string, args ...interface{}) {
+func WarnC(msg string, args ...any) {
 	printColon(3, msg, args...)
 }
-func ErrC(msg string, args ...interface{}) {
+func ErrC(msg string, args ...any) {
 	printColon(1, msg, args...)
 }
 
 // level 0 print
 // => message
-func Info0(msg string, args ...interface{}) {
+func Info0(msg string, args ...any) {
 	printColor0(4, msg, args...)
 }
-func Suc0(msg string, args ...interface{}) {
+func Suc0(msg string, args ...any) {
 	printColor0(2, msg, args...)
 }
-func Warn0(msg string, args ...interface{}) {
+func Warn0(msg string, args ...any) {
 	printColor0(3, msg, args...)
 }
-func Err0(msg string, args ...interface{}) {
+func Err0(msg string, args ...any) {
 	printColor0(1, msg, args...)
 }
 
 // level 1 print
 //   -> message
-func Info1(msg string, args ...interface{}) {
+func Info1(msg string, args ...any) {
 	printColor1(4, msg, args...)
 }
-func Suc1(msg string, args ...interface{}) {
+func Suc1(msg string, args ...any) {
 	printColor1(2, msg, args...)
 }
-func Warn1(msg string, args ...interface{}) {
+func Warn1(msg string, args ...any) {
 	printColor1(3, msg, args...)
 }
-func Err1(msg string, args ...interface{}) {
+func Err1(msg string, args ...any) {
 	printColor1(1, msg, args...)
 }
